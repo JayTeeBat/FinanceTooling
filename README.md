997823e8e16822473abf9c74427279afc89f13d8
Jacques <jacques.thomazo@forseepower.com>
2025-01-11 23:26:17 +0100

    Large update - functional full import of statement including date and amount of transactions. Still one small-ish incoherence on cumulated cost to be fixed.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # FinanceTracker
+
+This is a lightweight little piece of code to import LBP bank statements from pdf and
+export to Excel
--- src/import_statements.py
+++ src/import_statements.py
@@ -1,16 +1,35 @@
 from __future__ import annotations
 
+import decimal
 import os
 import re
-import tabula
+import datetime
 import pandas as pd
+from tqdm import tqdm
 import pdfplumber
+from decimal import Decimal
+
 
 FOLDER = r"E:\My Drive\Vie privee\Famille\Finances\Statements LaBanquePostale"
 
 FILE = r"releve_CCP1126215Y027_20240724.pdf"
 
 FILE_PATH = os.path.join(FOLDER, FILE)
+
+DAY_MONTH_PATTERN = re.compile(r"(0[1-9]|[12][0-9]|3[01])[/\-](0[1-9]|1[012])")
+REF_CREDIT_STR_LIST = [
+    "VIREMENT DE",
+    "CREDIT",
+]
+
+
+def import_all_statements(folder: str) -> dict[str, pd.DataFrame]:
+    dfs = {}
+    files = [file for file in os.scandir(folder) if file.is_file()]
+    for file in tqdm(files):
+        dfs[file.name] = import_statement(file.path)
+
+    return dfs
 
 
 def import_statement(filename: str) -> dict[pd.DataFrame] | pd.DataFrame:
@@ -32,27 +51,68 @@ def import_statement(filename: str) -> dict[pd.DataFrame] | pd.DataFrame:
                 (page_num, page_text)
             )  # Store with page number for reference
 
+    date_pattern = re.compile(
+        r"(((?:19|20)\d{2})[/\-]?(0[1-9]|1[012])[/\-]?(0[1-9]|[12][0-9]|3[01]))"
+    )
+    matches = date_pattern.findall(filename)
+    if len(matches) == 0:
+        print(f"No date was found in file {filename}")
+        # default year to use in datetime format later
+        date_year = 1
+    else:
+        # retrieving the year from the first match
+        # assumes the first date found is the actual date of the publication of the doc
+        date_year = matches[0][1]
     # Initialize lists to hold extracted transactions
     transactions = []
 
     # Define a regex pattern to extract transaction rows
     # (Date, Operation, Figure, Detail)
     transaction_pattern = re.compile(
-        r"\n(\d{2}\/\d{2})\s+"  # capturing date in format DD/MM
-        r"([^\n]*?)\s"          # capturing object
-        r"(-?(?:\d{1,3}|\d{1,3}(?:(?:\.|\s)\d{3})+),\s?\d{2}?)"     
+        r"\n(\d{2}/\d{2})\s+"  # capturing date in format DD/MM
+        r"([^\n]*?)\s"  # capturing object
+        r"(-?(?:\d{1,3}|\d{1,3}(?:(?:\.|\s)\d{3})+),\s?\d{2}?)"
         # capturing amount in format ' XXX XXX,XX\n'
-        r"(\n.*)"               # capturing additional description on next line
+        r"\n(.*)"  # capturing additional description on next line
     )
 
     # Parse each page's text for transactions
     for page_num, page_text in all_pages_text:
         matches = transaction_pattern.findall(page_text)
         for match in matches:
-            transactions.append(match)
+            # updating date format to include the year
+            # reallocation of the last transaction with new format
+            transactions.append(sanitize_transaction(date_year, match))
 
     # Convert extracted transactions into a DataFrame
-    columns = ["Date", "Operation", "Debit (€)", "Credit (€)"]
+    columns = ["Date", "Operation", "Amount", "Details"]
     transactions_df = pd.DataFrame(transactions, columns=columns)
 
     return transactions_df
+
+
+def sanitize_transaction(
+    date_year: str, transaction: re.match
+) -> tuple[datetime.date, str, decimal.Decimal, str]:
+
+    day, month = DAY_MONTH_PATTERN.findall(transaction[0])[0]
+    # spending by default
+    amount = -1 * Decimal(re.sub(r"[^\d\-.]", "", transaction[2].replace(",", ".")))
+
+    if any(
+        ref_str.lower() in transaction[1].lower() for ref_str in REF_CREDIT_STR_LIST
+    ):
+        amount *= -1
+
+    return (
+        datetime.date(int(date_year), int(month), int(day)),
+        transaction[1],
+        amount,
+        transaction[3],
+    )
+
+
+if __name__ == "__main__":
+    dfs = pd.concat([df for name, df in import_all_statements(FOLDER).items()], axis=0)
+    dfs.sort_values("Date", ascending=False, inplace=True)
+    dfs.to_excel("Export_LBP_data.xlsx")
